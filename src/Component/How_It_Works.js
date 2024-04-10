714e46546a711a0ea53f430c72398999e606bb82
aryannMobrilz <aryann@mobrilz.com>
2024-04-10 11:02:29 +0530

    change

--- src/Component/How_It_Works.js
+++ src/Component/How_It_Works.js
@@ -3,30 +3,51 @@ import React, { useState } from "react";
 const How_It_Works = () => {
   const [visible, setVisible] = useState(0)
   const work = () => {
-    if (visible == 0){
+    if (visible == 0 || visible == 2 || visible == 3  || visible == 4){
       setVisible(1)
      }else{
         setVisible(0)
       }
     }
+    const work2 = () => {
+      if (visible == 0 || visible == 1 || visible == 3  || visible == 4){
+        setVisible(2)
+       }else{
+          setVisible(0)
+        }
+      }
+      const work3 = () => {
+        if (visible == 0 || visible == 1 || visible == 2  || visible == 4){
+          setVisible(3)
+         }else{
+            setVisible(0)
+          }
+        }
+        const work4 = () => {
+          if (visible == 0 || visible == 1 || visible == 3  || visible == 2){
+            setVisible(4)
+           }else{
+              setVisible(0)
+            }
+          }
   return (
     <div>
       <div className="works-main container">
         <div className="work-left">
           <button onClick={work} className="work-box">box heading 1</button>
-          <div className="work-box">box heading 2</div>
-          <div className="work-box">box heading 3</div>
-          <div className="work-box">box heading 4</div>
+          <button onClick={work2} className="work-box">box heading 2</button>
+          <button onClick={work3} className="work-box">box heading 3</button>
+          <button onClick={work4} className="work-box">box heading 4</button>
         </div>
         <div className="work-right">
           {(visible == 1)?(
-          <div className="text-box">jfgbsjveufjbgukknuvkyudenvtkunhekyctnhnhb,nhgdkhdvn hjvnkhnsdknhvdshtus  hk lhglthlj;ldsjgls lej ljrluglersajglaslktgejligjlrejhglkjrelgjlrtjglgnjmlrwajlgjrlgw ejlriguoierjilj joiuwrieotldgjlrhglkanglkejrlkgjlugjl;ktejalyjeg;ljerlgjleagj sejrlgldrsjghljslkjglejrtglksdjrgl</div>):null}
-          {(visible == 1)?(
-          <div className="text-box">jfgbsjveufjbgukknuvkyudenvtkunhekyctnhnhb,nhgdkhdvn hjvnkhnsdknhvdshtus  hk lhglthlj;ldsjgls lej ljrluglersajglaslktgejligjlrejhglkjrelgjlrtjglgnjmlrwajlgjrlgw ejlriguoierjilj joiuwrieotldgjlrhglkanglkejrlkgjlugjl;ktejalyjeg;ljerlgjleagj sejrlgldrsjghljslkjglejrtglksdjrgl</div>):null}
-          {(visible == 1)?(
-          <div className="text-box">jfgbsjveufjbgukknuvkyudenvtkunhekyctnhnhb,nhgdkhdvn hjvnkhnsdknhvdshtus  hk lhglthlj;ldsjgls lej ljrluglersajglaslktgejligjlrejhglkjrelgjlrtjglgnjmlrwajlgjrlgw ejlriguoierjilj joiuwrieotldgjlrhglkanglkejrlkgjlugjl;ktejalyjeg;ljerlgjleagj sejrlgldrsjghljslkjglejrtglksdjrgl</div>):null}
-          {(visible == 1)?(
-          <div className="text-box">jfgbsjveufjbgukknuvkyudenvtkunhekyctnhnhb,nhgdkhdvn hjvnkhnsdknhvdshtus  hk lhglthlj;ldsjgls lej ljrluglersajglaslktgejligjlrejhglkjrelgjlrtjglgnjmlrwajlgjrlgw ejlriguoierjilj joiuwrieotldgjlrhglkanglkejrlkgjlugjl;ktejalyjeg;ljerlgjleagj sejrlgldrsjghljslkjglejrtglksdjrgl</div>):null}
+          <div className="text-box">hariom</div>):null}
+          {(visible == 2)?(
+          <div className="text-box">hello bhai</div>):null}
+          {(visible == 3)?(
+          <div className="text-box">1234567890</div>):null}
+          {(visible == 4)?(
+          <div className="text-box">abc</div>):null}
         </div>
       </div>
     </div>
