b45cfacbf95a674b3d6a526c14fe265be5c6bbac
aryannMobrilz <158017892+aryannMobrilz@users.noreply.github.com>
2024-10-15 14:26:00 +0530

    about us

--- src/Component/About_us.jsx
+++ src/Component/About_us.jsx
@@ -5,21 +5,22 @@ const About_us = () => {
   return (
     <div>
       <div className="">
-        <div className="bg-gray-50 pt-10">
-          <div className="container mx-auto px-2">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="bg-gray-50 pt-0 lg:pt-10">
+          <div className="container  mx-auto px-0 sm:px-2">
+                <h1 className="block lg:hidden"> About Us</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
               <div className="flex items-center justify-center">
                 <img
                   src={img}
                   alt="TaxiScout24"
-                  className="rounded-lg shadow-lg w-75 h-50"
+                  className="rounded-lg shadow-lg w-2/3 aspect-square"
                 />
               </div>
               <div className="flex flex-col justify-center">
-                <h2 className="text-3xl font-semibold text-gray-800 mb-4">
+                <h1 className="hidden lg:block text-3xl font-semibold text-gray-800 mb-4">
                   About Us
-                </h2>
-                <p className="text-gray-700 text-lg  text-center leading-relaxed mb-6 px-5">
+                </h1>
+                <p className="text-gray-700 text-lg   leading-relaxed mb-6 px-3 sm:px-0 text-justify xl:px-5 ">
                   Welcome to TaxiScout24, your global partner for innovative
                   taxi software solutions. We are proud to provide a pioneering
                   platform that supports taxi companies worldwide and offers
