a34b8e9e76cf2ff1f1b4a904858a70f95efb1cbc
aryannMobrilz <158017892+aryannMobrilz@users.noreply.github.com>
2024-10-29 11:56:52 +0530

    #locationButton fixed

--- src/Component/Landing.js
+++ src/Component/Landing.js
@@ -86,7 +86,7 @@ const Landing = () => {
                 <div class="">
                   <div className="relative px-10">
                     <InputItem type="source" />
-                    <div className="absolute top-0 right-1 translate-y-2 sm:translate-y-0 md:translate-y-1 lg:translate-y-[-10%] lg:right-0 translate-x-[-60%] ">
+                    <div className=" flex justify-end items-center translate-y-[-240%] translate-x-0 pr-5 sm:translate-y-[-240%] sm:translate-x-1 lg:translate-y-[-230%] lg:translate-x-1 xl:translate-y-[-180%] ">
                       <LocationButton setAddress={setAddress} />
                     </div>
                   </div>
