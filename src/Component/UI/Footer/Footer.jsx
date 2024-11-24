dc830988ae81e4774e68f598e72f04db12b7ce61
zarkofly <fly21882@gmail.com>
2024-11-24 15:56:39 +0100

    Update Footer.jsx

--- src/Component/UI/Footer/Footer.jsx
+++ src/Component/UI/Footer/Footer.jsx
@@ -1,31 +1,34 @@
 import { Typography } from "@material-tailwind/react";
 import logo from "../../../Images/logo.png"
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 
 
-
-
+export function Footer() {
+  const { t } = useTranslation();
 const LINKS = [
+
  
   {
-    title: "Company",
-    items: [{name:"About Us",slug:"about-us"},{name: "Our Partners", slug:"our-partner"}, {name:"Pricing", slug:"pricing"}, {name:"Our Team" , slug:"our-team"} , {name:"Gallery", slug:"gallery"}],
+    title: t('company'),
+    items: [{name:t('about_us'),slug:"about-us"},{name: t('our_partners'), slug:"our-partner"}, {name:t('pricing'), slug:"pricing"}, {name:t('our_team') , slug:"our-team"} , {name:t('gallery'), slug:"gallery"}],
   },
   {
-    title: "Resource",
-    items: [{name:"How We Work",slug:"how-it-works"},{name :"Blogs" , slug : "blogs"}],
+    title: t('resource'),
+    items: [{name: t('how_we_work'),slug:"how-it-works"},{name :t('blogs') , slug : "blogs"}],
   },
   {
-    title: "Contact",
-    items: [{name:"Contact Us", slug: "contact"}, {name:"FAQs" , slug : "faq"},{name: "Privacy Policy" , slug:"privacy-policy"}, {name:"Terms Of Use", slug: "term-of-use"} , {name:"Terms Of Services", slug: "term-of-services"}],
+    title: t('contact'),
+    items: [{name:t('contact_us'), slug: "contact"}, {name:t('faqs') , slug : "faq"},{name: t('privacy_policy') , slug:"privacy-policy"}, {name:t('terms_of_use'), slug: "term-of-use"} , {name:t('terms_of_services'), slug: "term-of-services"}],
   },
 ];
  
 const currentYear = new Date().getFullYear();
  
-export function Footer() {
+
   return (
+    
     <footer className="relative w-full bg-[#2c2c2c] ">
       <div className="mx-auto w-full max-w-7xl px-8">
         <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2">
@@ -62,8 +65,7 @@ export function Footer() {
             variant="small"
             className="mb-4 text-center font-normal text-white md:mb-0"
           >
-            &copy; {currentYear} <a href="https://material-tailwind.com/">Taxi Scout24</a>. All
-            Rights Reserved.
+            &copy; {currentYear} <a href="https://material-tailwind.com/">Taxi Scout24.</a> {t('all_rights_reserved')}
           </Typography>
           <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
             <Typography as="a" href="#" className="opacity-80 transition-opacity hover:opacity-100">
