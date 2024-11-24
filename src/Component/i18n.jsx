7a1318e8a7ebf1e5cfce014748f77b3baba7ef3b
zarkofly <fly21882@gmail.com>
2024-11-24 15:42:10 +0100

    Update i18n.jsx

--- src/Component/i18n.jsx
+++ src/Component/i18n.jsx
@@ -5,6 +5,14 @@ import { initReactI18next } from 'react-i18next';
 // Import your language translation files
 import en from './locales/en/landing.json';
 import es from './locales/es/landing.json';
+import ar from './locales/ar/landing.json';
+import de from './locales/de/landing.json';
+import fr from './locales/fr/landing.json';
+import it from './locales/it/landing.json';
+import pr from './locales/pr/landing.json';
+import rs from './locales/rs/landing.json';
+import sr from './locales/sr/landing.json';
+import tr from './locales/tr/landing.json';
 
 i18n
   .use(initReactI18next)
@@ -15,6 +23,30 @@ i18n
       },
       es: {
         translation: es,
+      },
+      ar: {
+        translation: ar,
+      },
+      de: {
+        translation: de,
+      },
+      fr: {
+        translation: fr,
+      },
+      it: {
+        translation: it,
+      },
+      pr: {
+        translation: pr,
+      },
+      rs: {
+        translation: rs,
+      },
+      sr: {
+        translation: sr,
+      },
+      tr: {
+        translation: tr,
       },
     },
     lng: 'en', // default language
