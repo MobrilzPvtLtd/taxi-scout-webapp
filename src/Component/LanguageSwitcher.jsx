6a915a4c36243fe6ce2db8356afd835257eb026a
zarkofly <fly21882@gmail.com>
2024-11-24 15:42:53 +0100

    Update LanguageSwitcher.jsx

--- src/Component/LanguageSwitcher.jsx
+++ src/Component/LanguageSwitcher.jsx
@@ -12,10 +12,16 @@ const LanguageSwitcher = () => {
     <div className='text-black '>
         <select onChange={changeLanguage} name="" defaultValue="en" id="">
 
-
-      <option value ='en'>English</option>
+  <option value ='en'>English</option>
       <option value ='es'>Español</option>
-      <option value ='ga'>German</option>
+      <option value ='de'>German</option>
+      <option value ='ar'>Arabic</option>
+      <option value ='fr'>French</option>
+      <option value ='it'>Italian</option>
+      <option value ='pr'>Portuguese</option>
+      <option value ='rs'>Russian</option>
+      <option value ='sr'>Serbian</option>
+      <option value ='tr'>Turkish</option>
         </select>
     </div>
   );
