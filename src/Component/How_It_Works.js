46016a5ac3b251d075d83cd8516ce8b3dfce86d8
zarkofly <fly21882@gmail.com>
2024-11-30 14:53:11 +0100

    Update How_It_Works.js

--- src/Component/How_It_Works.js
+++ src/Component/How_It_Works.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 const How_It_Works = () => {
+  const { t } = useTranslation();
   const [visible, setVisible] = useState(1)
   const work = () => {
     if (visible == 0 || visible == 2 || visible == 3  || visible == 4){
@@ -34,23 +36,23 @@ const How_It_Works = () => {
     <div id="how_it_works">
       
       <div className="bg-[#00000080] p-4 rounded-lg shadow-xl py-2 mt-5">
-      <h1 className="mt-10 text-[50px] font-bold text-white ">How it Works ?</h1> 
+      <h1 className="mt-10 text-[50px] font-bold text-white ">{t(`work_text1`)}</h1> 
         <div className="flex gap-5">
         <div className="work-left">
-          <button onClick={work} className="work-box mb-2">Type of trip</button>
-          <button onClick={work2} className="work-box mb-2">Enter your details</button>
-          <button onClick={work3} className="work-box mb-2">Choose your taxi</button>
-          <button onClick={work4} className="work-box mb-2">Make payment</button>
+          <button onClick={work} className="work-box mb-2">{t(`work_text2`)}</button>
+          <button onClick={work2} className="work-box mb-2">{t(`work_text3`)}</button>
+          <button onClick={work3} className="work-box mb-2">{t(`work_text4`)}</button>
+          <button onClick={work4} className="work-box mb-2">{t(`work_text5`)}</button>
         </div>
         <div className="mt-5 work-right">
           {(visible == 1)?(
-          <div className="text-box">Choose your trip One way, Round Trip, Airport Cabs</div>):null}
+          <div className="text-box">{t(`work_text6`)}</div>):null}
           {(visible == 2)?(
-          <div className="text-box">Submit your details regarding your trip like date, time and location</div>):null}
+          <div className="text-box">{t(`work_text7`)}</div>):null}
           {(visible == 3)?(
-          <div className="text-box">Choose which type of cab you want for your trip Audi, BMW etc</div>):null}
+          <div className="text-box">{t(`work_text8`)}</div>):null}
           {(visible == 4)?(
-          <div className="text-box">Pay before or after trip. We accept multiple payment mode Cash, Credit Card, Debit Card, Netbanking</div>):null}
+          <div className="text-box">{t(`work_text9`)}</div>):null}
         </div>
         </div>
       </div>
