94e26607fe13a8448c8a3f359b425a51662e9888
zarkofly <fly21882@gmail.com>
2024-11-30 13:34:33 +0100

    Update About_us.jsx

--- src/Component/About_us.jsx
+++ src/Component/About_us.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import img from "../Images/serve_globally2.jpg";
 import { Skeleton } from "antd";
-
+import { useTranslation } from "react-i18next";
 const About_us = () => {
+  const { t } = useTranslation();
   const [loading, setLoading] = useState(true);
   setTimeout(() => {
     setLoading(false);
@@ -25,23 +26,10 @@ const About_us = () => {
               </div>
               <div className="flex flex-col justify-center">
                 <h1 className="hidden lg:block text-3xl font-semibold text-gray-800 mb-4">
-                  About Us
+                  {t('about_us')}
                 </h1>
                 <p className="text-gray-700 text-lg   leading-relaxed mb-6 px-3 sm:px-0 text-justify xl:px-5 ">
-                  Welcome to TaxiScout24, your global partner for innovative
-                  taxi software solutions. We are proud to provide a pioneering
-                  platform that supports taxi companies worldwide and offers
-                  passengers a seamless travel experience. Our company was
-                  founded with the vision to revolutionize the taxi industry and
-                  create a connected world where taxi companies operate more
-                  efficiently and passengers travel more comfortably. With our
-                  customized software and app offering, we enable taxi companies
-                  of all sizes and orientations to optimize their operations and
-                  expand their reach. Our state-of-the-art software offers a
-                  wealth of features specifically designed to simplify the daily
-                  operations of taxi companies. From order management to fleet
-                  tracking, we provide a comprehensive solution that allows our
-                  customers to work more efficiently and improve their service.
+                {t('about_text1')}
                 </p>
               </div>
             </div>
