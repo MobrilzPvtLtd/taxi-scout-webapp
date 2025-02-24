d315e8de0c721025fdbf8699a7755f981bcdefca
Anjali Singh <anjali@mobrilz.com>
2025-02-24 14:49:19 +0530

    new changes

--- src/Component/Pricing.js
+++ src/Component/Pricing.js
@@ -29,19 +29,19 @@ const SubscriptionPage = () => {
           <Skeleton active />
         </div>
       ) : (
-        <section class="bg-gradient-to-r from-yellow-500 to-orange-500 py-12 mt-16  ">
-          <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="text-center mb-12">
-              <div class="text-4xl font-extrabold text-black sm:text-5xl">
+        <section className="bg-gradient-to-r from-yellow-500 to-orange-500 py-12 mt-16  ">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="text-center mb-12">
+              <div className="text-4xl font-extrabold text-black sm:text-5xl">
                 <h1> {t(`pricing_text1`)} </h1>
               </div>
             
             </div>
 
-            <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
               {price?.map((item, index) => (
                 <div
-                  class="bg-white bg-opacity-10 rounded-lg shadow-lg p-6 relative overflow-hidden"
+                  className="bg-white bg-opacity-10 rounded-lg shadow-lg p-6 relative overflow-hidden"
                   key={index}
                 >
                   {/* <div class="absolute top-0 right-0 m-4">
@@ -49,22 +49,22 @@ const SubscriptionPage = () => {
                       {item.package_name}
                     </span>
                   </div> */}
-                  <div class="mb-8">
-                    <h3 class="text-xs  sm:text-sm md:text-lg lg:text-xl font-semibold text-black">
+                  <div className="mb-8">
+                    <h3 className="text-xs  sm:text-sm md:text-lg lg:text-xl font-semibold text-black">
                       {item.package_name}
                     </h3>
                     {/* <p class="mt-4 text-purple-200">Perfect for individuals and small teams.</p> */}
                   </div>
-                  <div class="mb-8">
-                    <span class="text-5xl font-extrabold text-black">
-                      ${item.amount}
+                  <div className="mb-8">
+                    <span className="text-5xl font-extrabold text-black">
+                      {item.amount}
                     </span>
-                    <span class="text-xl font-medium text-black-300">/{t(`pricing_text3`)}</span>
+                    <span className="text-xl font-medium text-black-300">/{t(`pricing_text3`)}</span>
                   </div>
-                  <ul class="mb-8 space-y-4 text-gray-700">
-                    <li class="flex items-center">
+                  <ul className="mb-8 space-y-4 text-gray-700">
+                    <li className="flex items-center">
                       <svg
-                        class="h-6 w-6 text-green-400 mr-2"
+                        className="h-6 w-6 text-green-400 mr-2"
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
@@ -79,9 +79,9 @@ const SubscriptionPage = () => {
                       </svg>
                       <span>{item.number_of_drivers} {t(`pricing_text4`)}</span>
                     </li>
-                    <li class="flex items-center">
+                    <li className="flex items-center">
                       <svg
-                        class="h-6 w-6 text-green-400 mr-2"
+                        className="h-6 w-6 text-green-400 mr-2"
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
@@ -100,7 +100,7 @@ const SubscriptionPage = () => {
                   <Link
                     to="/signup"
                     onClick={()=>setUserType(t('company'))}
-                    class="block w-full py-3 px-6 text-center rounded-md text-black font-semibold bg-gradient-to-r from-yellow-600 to-[#ffd91c] hover:from-yellow-700 hover:to-orange-400"
+                    className="block w-full py-3 px-6 text-center rounded-md text-black font-semibold bg-gradient-to-r from-yellow-600 to-[#ffd91c] hover:from-yellow-700 hover:to-orange-400"
                   >
                      {t(`pricing_text6`)}
                   </Link>
