6bae76480ecafb83870e4b1a7bf86253cfbfa793
zarkofly <fly21882@gmail.com>
2025-04-13 19:27:28 +0200

    Update register_user.php

--- register_user.php
+++ register_user.php
@@ -1,7 +1,7 @@
 <?php
 // Handle CORS preflight request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("Access-Control-Allow-Origin: http://localhost:3000");
+    header("Access-Control-Allow-Origin: https://taxiscout24.com");
     header("Access-Control-Allow-Methods: POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type");
     header("Access-Control-Max-Age: 86400"); // Cache preflight response for 24 hours
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 }
 
 // Set CORS headers for actual request
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin:https://taxiscout24.com");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
