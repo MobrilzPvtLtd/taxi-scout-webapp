882273e72968fc28896d1b1e5e4c86a82f38f9c2
zarkofly <fly21882@gmail.com>
2025-05-04 20:04:31 +0200

    Update i18n.jsx

--- src/Component/i18n.jsx
+++ src/Component/i18n.jsx
@@ -3,10 +3,10 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 // Import your language translation files
+import de from './locales/de/landing.json';
 import en from './locales/en/landing.json';
 import es from './locales/es/landing.json';
 import ar from './locales/ar/landing.json';
-import de from './locales/de/landing.json';
 import fr from './locales/fr/landing.json';
 import it from './locales/it/landing.json';
 import pr from './locales/pr/landing.json';
@@ -18,17 +18,18 @@ i18n
   .use(initReactI18next)
   .init({
     resources: {
-      en: {
-        translation: en,
+      de: {
+        translation: de,
       },
+     
       es: {
         translation: es,
       },
       ar: {
         translation: ar,
       },
-      de: {
-        translation: de,
+     en: {
+        translation: en,
       },
       fr: {
         translation: fr,
@@ -49,8 +50,8 @@ i18n
         translation: tr,
       },
     },
-    lng: 'en', // default language
-    fallbackLng: 'en', // fallback language if translation is missing
+    lng: 'de', // default language
+    fallbackLng: 'de', // fallback language if translation is missing
     interpolation: {
       escapeValue: false, // React already does escaping
     },
